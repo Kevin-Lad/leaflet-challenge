ac5726aec797ba989ba77c9733cb00595043c14a
Kevin-Lad <kevinladisheff@gmail.com>
2023-09-21 16:09:57 -0700

    earthquake color updated

---
+++ static/js/logic.js
@@ -0,0 +1,87 @@
+// Store the API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+
+// Perform a GET request to the query URL.
+d3.json(queryUrl).then(function (data) {
+
+  console.log(data.features);
+
+  // Using the features array sent back in the API data, create a GeoJSON layer, and add it to the map.
+  function useFeature(feature, layer) {
+  //   layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  // }
+       layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + feature.geometry.coordinates[2] + "</p>");
+      }
+  function chooseColor(depth) {
+   
+
+    if (depth > 90) return 'red';
+    else if (depth > 70) return  'orange';
+    else if (depth > 50) return 'yellow';
+    else if (depth > 30) return 'greenyellow';
+    else return 'lime';
+    
+
+  };
+
+  let earthquakes = L.geoJSON(data.features, {
+
+    style: function(feature) {
+      console.log(feature.geometry.coordinates[2])
+      return {
+        color: "black",
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
+        fillOpacity: 0.5,
+        weight: 1.5
+      }
+    },
+    
+    onEachFeature: useFeature,
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+  }
+  })
+
+  createMap(earthquakes);
+});
+
+
+function createMap(earthquakes) {
+  
+  // Create the base layers.
+  
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlays object.
+  let overlayMaps = {
+    Earthquakes: earthquakes,
+  };
+  
+  // Create a new map.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control that contains our baseMaps.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+}
